18d1574a9493caef2c47cd592420f58a23429e5b
ChrisCooper0 <christopher.cooper@mail.com>
2022-04-12 22:01:59 +0100

    add nodemon and refactor PUT

--- client/components/TodoList.tsx
+++ client/components/TodoList.tsx
@@ -2,55 +2,64 @@ import React from "react";
 import styled from "styled-components";
 
 const TodoListWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   margin-top: 2rem;
-  li {
-    margin-bottom: 0.75rem;
+
+  .todos {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 1.5rem;
   }
+`;
+
+const Title = styled.p`
+  font-size: 1.1rem;
+  text-decoration: ${({ completed }) => (completed ? "line-through" : "none")};
+  color: ${({ completed }) => (completed ? "grey" : "black")};
 `;
 
 const Button = styled.button`
   border: none;
-  border-radius: 5px;
-  padding: 0.1rem 0.3rem;
-  background-color: #f5f5f5;
+  text-align: center;
+  border-radius: 50%;
+  margin-left: 1rem;
+  padding: 0.2rem 0.4rem;
   transition: all 0.2s ease-in-out;
   cursor: pointer;
-  margin-left: 1rem;
+  color: #c96e6e;
+  background-color: #f5f5f5;
   &:hover {
     background-color: #e5e5e5;
     color: #ce9a9a;
   }
 `;
 
+const Input = styled.input`
+  margin-left: 1rem;
+`;
+
 interface ITodo {
-  todo: { id: string; title: string }[];
+  todo: { id: string; title: string; completed: boolean }[];
   handleDelete: (id: string) => void;
-  handleUpdate: (id: string, title: string) => void;
+  handleUpdate: (id: string, title: string, completed: boolean) => void;
 }
 
 const TodoList = ({ todo, handleDelete, handleUpdate }: ITodo) => {
   return (
     <TodoListWrapper>
-      <ol>
-        {todo.map(({ id, title }) => (
-          <li key={id}>
-            {title}
-            <Button type="button" onClick={() => handleDelete(id)}>
-              x
-            </Button>
-            <Button
-              type="button"
-              className="update"
-              onClick={() => handleUpdate(id, title)}
-            >
-              Update
-            </Button>
-          </li>
-        ))}
-      </ol>
+      {todo.map(({ id, title, completed }) => (
+        <div className="todos" key={id}>
+          <Title completed={completed}>{title}</Title>
+          <Button type="button" onClick={() => handleDelete(id)}>
+            X
+          </Button>
+          <Input
+            type="checkbox"
+            checked={completed}
+            onClick={() => handleUpdate(id, title, completed)}
+          />
+        </div>
+      ))}
     </TodoListWrapper>
   );
 };
