75529b288c8a68cc7fddb4d2497ab29823652e3a
ChrisCooper0 <christopher.cooper@mail.com>
2022-06-25 14:53:34 +0100

    tiny update

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use("/api/todos", todoRoutes);
 
 // Express port
 const PORT = 3001;
+
 app.listen(PORT, () => {
-  console.log("Server is running on port 3001");
+  console.log(`Server is running on port ${PORT}`);
 });
